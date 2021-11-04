8d0d9f3dab5ae8d3f65758877439eb4b2652983c
Quentin <quentin.lejuez@gmail.com>
2021-11-04 15:27:35 +0100

    SFML - Movements

--- 4 - Cours 04-11-2021 - Copie/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ 4 - Cours 04-11-2021 - Copie/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,11 +4,26 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+
+
+	sf::Font font;
+	font.loadFromFile("Game Of Squids.ttf");
+
+	sf::RenderWindow window(sf::VideoMode(1280, 720), "The Witcher 3");
+	window.setVerticalSyncEnabled(true);
+	sf::RectangleShape shape(sf::Vector2f(100, 50));
+	shape.setFillColor(sf::Color::Red);
+	shape.setOutlineThickness(4);
+	shape.setOutlineColor(sf::Color::White);
+
+
+	sf::Text text("hello", font);
+	text.setCharacterSize(30);
+	text.setStyle(sf::Text::Bold);
+	text.setFillColor(sf::Color::Yellow);
 
 	while (window.isOpen())
 	{
@@ -16,11 +31,39 @@ int main()
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
-				window.close();
+			window.close();
+
+			auto pos = shape.getPosition();
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+			{
+				pos.x--;
+				shape.setPosition(pos);
+				//shape.move(-1,0);
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+			{
+				pos.x++;
+				shape.setPosition(pos);
+				//shape.move(1, 0);
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+			{
+				pos.y--;
+				shape.setPosition(pos);
+				//shape.move(0, -1);
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+			{
+				pos.y++;
+				shape.setPosition(pos);
+				//shape.move(0, 1);
+			}
+			
 		}
 
 		window.clear();
 		window.draw(shape);
+		window.draw(text);
 		window.display();
 	}
 
