182f9d35c5487f03b557632e3bcad4958aec1c9c
Quentin <quentin.lejuez@gmail.com>
2021-11-09 16:58:27 +0100

    Cours 5

---
+++ 4 - Cours 04-11-2021 - Copie/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1/Bullet.hpp
@@ -0,0 +1,25 @@
+#pragma once
+/*
+#include "SFML/Graphics/RenderWindow.hpp"
+#include "SFML/Graphics/CircleShape.hpp"
+
+class Bullet {
+public:
+	sf::CircleShape b;
+
+	std::vector<float> px;
+	std::vector<float> py;
+
+	std::vector<float> dx;
+	std::vector<float> dy;
+
+
+	Bullet();
+
+	void update(double dt);
+
+	void draw(sf::RenderWindow& win);
+
+	void create(float _px, float _py, float _dx, float _dy);
+};
+*/
